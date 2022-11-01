947dfdcfb5a966ba5a5b429201f62b39869e9525
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-31 20:45:06 -0400

    Startup

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -7,6 +7,7 @@ namespace FilmesApi.Models {
         public string? Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatorio")]
         public string? Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         public string? Genero { get; set; }
         [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 e bi naxuni 600 minutos")]
         public int Duracao { get; set; }
