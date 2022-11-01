7df54d9d142aa1b93947701ea088936a7a692c39
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-31 20:21:52 -0400

    Controller e classe models

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesApi.Controllers 
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+
+
+        public void AdicionarFilme(Filme filme) 
+        {
+            filmes.Add(filme);
+        }
+    }
+}
