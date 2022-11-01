f6d2d9df8b8992028f779e7f9804366cd7e13170
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-31 20:39:28 -0400

    mensagem na model

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,9 +1,14 @@
-﻿namespace FilmesApi.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models {
     public class Filme 
     {
+        [Required(ErrorMessage ="O campo titulo é obrigatorio")]
         public string? Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
         public string? Diretor { get; set; }
         public string? Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 e bi naxuni 600 minutos")]
         public int Duracao { get; set; }
 
     }
